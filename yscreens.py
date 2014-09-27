62245701d061ee8c713272217e27fcc3421f73cf
Nama <mrt690@gmail.com>
2014-09-27 17:04:55 +0200

    Handling whitespace in filename for links

--- yscreens.py
+++ yscreens.py
@@ -10,23 +10,23 @@ from balloons import WindowsBalloonTip
 config = configparser.ConfigParser()
 config.read('yscreens.ini')
 
-try: #Check for script-parameters
+try:  # Check for script-parameters
     m = sys.argv[1]
-except: #If no parameters are set
+except:  # If no parameters are set
     m = None
 if m:
     screen_count = int(config.get('yscreens', 'count'))
-    upload_file = 'y%s.jpg' %screen_count
+    upload_file = 'y%s.jpg' % screen_count
     ImageGrab.grab().save(upload_file, quality="high")
     screen_count += 1
     config.set('yscreens', 'count', str(screen_count))
     with open('yscreens.ini', 'w') as configfile:
         config.write(configfile)
-else: #If no parameters are set
-    upload_file = input('Type in the Filepath: ').replace('"', '').replace(' ', '%20')
+else:  # If no parameters are set
+    upload_file = input('Type in the Filepath: ').replace('"', '')
     upload_file_name = str(upload_file.split('\\').pop())
     m = 'file'
-#Connect to the ssh server
+# Connect to the ssh server
 t = paramiko.Transport((config.get('yscreens', 'host'), int(config.get('yscreens', 'port'))))
 key_file = config.get('yscreens', 'key', fallback=None)
 if key_file != '':
@@ -37,13 +37,13 @@ else:
     t.connect(username=config.get('yscreens', 'username'), password=password, hostkey=None)
 sftp = paramiko.SFTPClient.from_transport(t)
 
-if m == 'screen': #If script-parameter 'screen', upload the screenshot, put the link in to clipboard, delete screenshot
+if m == 'screen':  # If script-parameter 'screen', upload the screenshot, put the link in to clipboard, delete screenshot
     sftp.put(upload_file, config.get('yscreens', 'rmpath') + upload_file)
     pyperclip.copy(config.get('yscreens', 'url') + upload_file)
     os.remove(upload_file)
-else: #Upload the given file, put link in to clipboard
+else:  # Upload the given file, put link in to clipboard
     sftp.put(upload_file, config.get('yscreens', 'rmpath') + upload_file_name)
-    pyperclip.copy(config.get('yscreens', 'url') + upload_file_name)
+    pyperclip.copy(config.get('yscreens', 'url') + upload_file_name.replace(' ', '%20'))
     upload_file = upload_file_name
 t.close
 
